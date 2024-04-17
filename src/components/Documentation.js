91a854ef0458e04da66c477dda8966cfff60c0ff
IlhamAufal <l200214071@student.ums.ac.id>
2024-04-17 13:04:26 +0700

    penambahan dokumentasi ver baru

--- src/components/Documentation.js
+++ src/components/Documentation.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import PopUp from "./PopUp";
-import "../style/landingpage.css";
+import "../style/gallery-grid.css"; // Pastikan path-nya sesuai dengan lokasi file CSS
+import park from "../asset/park.jpg";
+import bridge from "../asset/bridge.jpg";
+import coast from "../asset/coast.jpg";
+import tunnel from "../asset/tunnel.jpg";
+import rails from "../asset/rails.jpg";
+import traffic from "../asset/traffic.jpg";
+import rocks from "../asset/rocks.jpg";
+import benches from "../asset/benches.jpg";
+import sky from "../asset/sky.jpg";
 
 const Documentation = () => {
   const navigate = useNavigate();
@@ -10,7 +19,6 @@ const Documentation = () => {
   return (
     <div
       style={{
-        backgroundColor: "red",
         padding: "30px",
         borderRadius: "20px",
         margin: "auto",
@@ -19,66 +27,65 @@ const Documentation = () => {
         maxWidth: "1400px",
       }}
     >
-      <Container style={{ maxWidth: "1200px" }}>
-        <div className="mySubMenu1" style={{ marginBottom: "30px" }}>
+      <Container>
+      <link rel="stylesheet" href="gallery-grid.css" />
+      <div className="container gallery-container">
+        <div className="tz-gallery">
+        <div className="mySubMenu1" style={{ marginBottom: "30px", maxWidth:'50%'}}>
           Dokumentasi
         </div>
-          <div className="d-flex flex-wrap" style={{maxHeight:'600px', maxWidth:'1200px'}}>
-            <Link to="/">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                className="w-100 shadow-1-strong rounded mb-4"
-                />
-            </Link>
-
-           <Link to="/">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                className="w-100 shadow-1-strong rounded mb-4"
-                />
-            </Link>
-
-           <Link to="/">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                className="w-100 shadow-1-strong rounded mb-4"
-                />
-            </Link>
-
-           <Link to="/">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                className="w-100 shadow-1-strong rounded mb-4"
-                />
-            </Link>
+          <div className="row">
+            <div className="col-sm-6 col-md-4">
+              <a className="lightbox" href={park}>
+                <img src={park} alt="Park" />
+              </a>
+            </div>
+            <div className="col-sm-6 col-md-4">
+              <a className="lightbox" href={bridge}>
+                <img src={bridge} alt="Bridge" />
+              </a>
+            </div>
+            <div className="col-sm-12 col-md-4">
+              <a className="lightbox" href={tunnel}>
+                <img src={tunnel} alt="Tunnel" />
+              </a>
+            </div>
+            <div className="col-sm-6 col-md-4">
+              <a className="lightbox" href={coast}>
+                <img src={coast} alt="Coast" />
+              </a>
+            </div>
+            <div className="col-sm-6 col-md-4">
+              <a className="lightbox" href={rails}>
+                <img src={rails} alt="Rails" />
+              </a>
+            </div>
+            <div className="col-sm-6 col-md-4">
+              <a className="lightbox" href={traffic}>
+                <img src={traffic} alt="Traffic" />
+              </a>
+            </div>
+            <div className="col-sm-6 col-md-4">
+              <a className="lightbox" href={rocks}>
+                <img src={rocks} alt="Rocks" />
+              </a>
+            </div>
+            <div className="col-sm-6 col-md-4">
+              <a className="lightbox" href={benches}>
+                <img src={benches} alt="Benches" />
+              </a>
+            </div>
+            <div className="col-sm-6 col-md-4">
+              <a className="lightbox" href={sky}>
+                <img src={sky} alt="Sky" />
+              </a>
+            </div>
           </div>
-
-          <div className="col-lg-4 mb-4 mb-lg-0">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-              className="w-100 shadow-1-strong rounded mb-4"
-            />
-
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-              className="w-100 shadow-1-strong rounded mb-4"
-            />
-          </div>
-
-          <div className="col-lg-4 mb-4 mb-lg-0">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-              className="w-100 shadow-1-strong rounded mb-4"
-            />
-
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-              className="w-100 shadow-1-strong rounded mb-4"
-            />
         </div>
-        <div style={{ marginLeft: "43%" }}>
+      </div>
+        {/* <div style={{ marginLeft: "43%" }}>
           <PopUp />
-        </div>
+        </div> */}
       </Container>
     </div>
   );
